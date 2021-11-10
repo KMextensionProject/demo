844dbd4bf4289a3c8107cefda85ce0f07abd6d3b
Martin Krajcovic <mar.krajc@gmail.com>
2021-11-11 00:11:31 +0100

    added implementation of UserDetailsService and JdbcTemplate

--- src/main/java/com/demo/security/database/AuthJdbcTemplate.java
+++ src/main/java/com/demo/security/database/AuthJdbcTemplate.java
@@ -1,17 +1,43 @@
 package com.demo.security.database;
 
+import java.util.List;
+
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
+import com.demo.security.user.DemoUser;
+import com.demo.security.user.DemoUserRowMapper;
+// TODO: do tohto implementovat user cache
 @Component
 public class AuthJdbcTemplate extends JdbcTemplate {
+
+	private static final String USER_AUTH_LEFT_JOIN = "SELECT A.*, B.authority FROM users A LEFT JOIN authorities B ON A.username = B.username";
+	private static final String IS_ACTIVE = "A.enabled = 1";
+	
+	private static final String ALL_USERS_QUERY = USER_AUTH_LEFT_JOIN + " WHERE " + IS_ACTIVE;
+	private static final String USER_BY_ID_QUERY = USER_AUTH_LEFT_JOIN + " WHERE A.id = ? AND " + IS_ACTIVE;
+	private static final String USER_BY_USERNAME_QUERY = USER_AUTH_LEFT_JOIN + " WHERE A.username = ? AND " + IS_ACTIVE;
+
+	@Autowired
+	private DemoUserRowMapper userRowMapper;
 
 	@Autowired
 	public AuthJdbcTemplate(final DataSource dataSource) {
 		super(dataSource);
 	}
 
+	public List<DemoUser> getUsers() {
+		return query(ALL_USERS_QUERY, userRowMapper);
+	}
+
+	public DemoUser getUserById(int id) {
+		return queryForObject(USER_BY_ID_QUERY, userRowMapper, id);
+	}
+
+	public DemoUser getUserByUsername(String username) {
+		return queryForObject(USER_BY_USERNAME_QUERY, userRowMapper, username);
+	}
 }
--- src/main/java/com/demo/security/user/DemoUserDetailsService.java
+++ src/main/java/com/demo/security/user/DemoUserDetailsService.java
@@ -1,16 +1,31 @@
 package com.demo.security.user;
 
+import java.util.Objects;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
+import com.demo.security.database.AuthJdbcTemplate;
+
 @Component
 public class DemoUserDetailsService implements UserDetailsService {
 
+	@Autowired
+	private AuthJdbcTemplate jdbcTemplate;
+
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		return null;
+		DemoUser user = jdbcTemplate.getUserByUsername(username);
+		validateUser(user);
+		return user;
 	}
 
+	private void validateUser(DemoUser user) {
+		if (Objects.isNull(user) || Objects.isNull(user.getUsername())) {
+			throw new UsernameNotFoundException("User doesn't exist.");
+		}
+	}
 }
