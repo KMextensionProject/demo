ba1812d4b7956ca61288aab06fc1f658ddace98b
Martin Krajcovic <mar.krajc@gmail.com>
2021-11-14 15:14:46 +0100

    implementation of custom AuthenticationProvider

--- src/main/java/com/demo/security/DemoAuthenticationProvider.java
+++ src/main/java/com/demo/security/DemoAuthenticationProvider.java
@@ -2,9 +2,14 @@ package com.demo.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+
+import com.demo.security.user.DemoUserDetailsService;
 
 @Component
 public class DemoAuthenticationProvider implements AuthenticationProvider {
@@ -12,14 +17,27 @@ public class DemoAuthenticationProvider implements AuthenticationProvider {
 	@Autowired
 	private PasswordEncoder passwordEncoder;
 
+	@Autowired
+	private DemoUserDetailsService userDetailsService;
+
 	@Override
 	public Authentication authenticate (final Authentication authentication) {
-		return null;
+		String username = authentication.getName();
+		String password = String.valueOf(authentication.getCredentials());
+
+		UserDetails user = userDetailsService.loadUserByUsername(username);
+
+		boolean passwordMatches = passwordEncoder.matches(password, user.getPassword());
+		if (!passwordMatches) {
+			throw new BadCredentialsException("Wrong username or password");
+		}
+
+		return new UsernamePasswordAuthenticationToken(username, password, user.getAuthorities());
 	}
 
 	@Override
 	public boolean supports (Class<?> authentication) {
-		return false;
+		return authentication.equals(UsernamePasswordAuthenticationToken.class);
 	}
 
 }
