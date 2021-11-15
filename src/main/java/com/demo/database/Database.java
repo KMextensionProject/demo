bba518ea7c7f9e6ee6e38a4cd553b16a5dce1845
Martin Krajcovic <mar.krajc@gmail.com>
2021-11-15 21:49:25 +0100

    added database package where the more convenient api will be placed

---
+++ src/main/java/com/demo/database/Database.java
@@ -0,0 +1,13 @@
+package com.demo.database;
+
+public class Database {
+
+	// TODO: implement db api for easier and more convenient query construction
+	// also dont forget, that this needs different datasource, so note the
+	// existing authJdbcTemplate 
+
+	private class Select {
+
+	}
+
+}
